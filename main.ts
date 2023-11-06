6e5c2cc1c8cb6369fe0bc13491106ca74b2a9cb4
Simon Schmid <s_schmid@gmx.at>
2023-11-06 12:07:26 +0100

    CHANGES: update to build files and functions

--- main.ts
+++ main.ts
@@ -3,41 +3,52 @@ Missimo workshop kit extension
 */
 
 
-//% weight=0 icon="\uf1b6" color=#87CEEB
+//% weight=0 icon="\uf1b9" color=#25db9c
 namespace Missimo {
-    //% blockId=missimo_ultrasonic block="Ultraschall Distanz|Trigger %Trigger|Echo %Echo"
-    //% color="#87CEEB"
+
+    //% blockId=missimo_ultrasonic
+    //% block="Messe Distanz|Trigger $trigger|Echo $echo"
+    //% blockHidden=true
+    //% color=#25db9c
     //% weight=100
     //% blockGap=10
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function ultraschall_distanz(Trigger: DigitalPin, Echo: DigitalPin): number {
+    //% trigger.fieldEditor="gridpicker"
+    //% trigger.fieldOptions.columns=4
+    //% trigger.defl=DigitalPin.P8
+    //% echo.fieldEditor="gridpicker"
+    //% echo.fieldOptions.columns=4
+    //% echo.defl=DigitalPin.P2
+    export function measure_distance(trigger: DigitalPin, echo: DigitalPin = DigitalPin.P2): number {
         // send trigger pulse
-        pins.setPull(Trigger, PinPullMode.PullNone);
-        pins.digitalWritePin(Trigger, 0);
+        pins.setPull(trigger, PinPullMode.PullNone);
+        pins.digitalWritePin(trigger, 0);
         control.waitMicros(2);
-        pins.digitalWritePin(Trigger, 1);
+        pins.digitalWritePin(trigger, 1);
         control.waitMicros(15);
-        pins.digitalWritePin(Trigger, 0);
+        pins.digitalWritePin(trigger, 0);
 
         // read pulse and convert to cm
-        let d = pins.pulseIn(Echo, PulseValue.High, 23200);
+        let d = pins.pulseIn(echo, PulseValue.High, 23200);
         return Math.floor(d / 58.2);
     }
 
-    //% blockId=missimo_ultrasonic_robot block="Ultraschall_Distanz_Roboter"
-    //% color="#87CEEB"
+    //% blockId=missimo_ultrasonic_robot
+    //% block="Messe Distanz in cm"
+    //% block.loc.en="measure distance in cm"
+    //% jsdoc.loc.en="measure distance in cm"
+    //% color=#25db9c
     //% weight=100
     //% blockGap=10
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=2
-    export function ultraschall_distanz_roboter(): number {
-        let oldDist = ultraschall_distanz(DigitalPin.P8, DigitalPin.P2);
+    export function measure_distance_avg(): number {
+        let oldDist = measure_distance(DigitalPin.P8, DigitalPin.P2);
         let avg = oldDist;
         for (let index = 0; index <= 10; index++)
         {
-            let dist = Ultraschall_Distanz(8, 2);
+            let dist = measure_distance(DigitalPin.P8, DigitalPin.P2);
             avg = (0.8 * oldDist) + (0.2 * dist);
             control.waitMicros(10);
         }
         return Math.floor(avg);
     }
+
 }
--- test.ts
+++ test.ts
@@ -1,2 +1,1 @@
-// put tests here
-// NOTE: this will not be compiled when this package is used as a library
+// tests go here; this will not be compiled when this package is used as an extension.
